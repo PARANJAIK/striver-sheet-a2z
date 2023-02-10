26a81fa27caccb4d0fca2d6de212b87de85a56b6
PARANJAIK <97453375+PARANJAIK@users.noreply.github.com>
2023-02-10 11:46:32 +0530

    Update InorderPredecessorAndSuccessorInBST.cpp

--- InorderPredecessorAndSuccessorInBST.cpp
+++ InorderPredecessorAndSuccessorInBST.cpp
@@ -7,31 +7,31 @@
     // move to the left, and if it cant be the successor it means its left cant be the successor so we move to the right.
     // we do some very similar stuff for predecessor too.
 
-// void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
-// {
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+{
 
-// // Your code goes here
-// pre = NULL;
-// suc = NULL;
-// Node* cur = root;
-// while(cur){
-//     if(cur->key <= key){
-//         cur = cur->right;
-//     }
-//     else{
-//         suc = cur;
-//         cur = cur->left;
-//     }
-// }
-// cur = root;
-// while(cur){
-//     if(cur->key >= key){
-//         cur = cur->left;
-//     }
-//     else{
-//         pre = cur;
-//         cur = cur->right;
-//     }
-// }
-// return;
-// }
+// Your code goes here
+pre = NULL;
+suc = NULL;
+Node* cur = root;
+while(cur){
+    if(cur->key <= key){
+        cur = cur->right;
+    }
+    else{
+        suc = cur;
+        cur = cur->left;
+    }
+}
+cur = root;
+while(cur){
+    if(cur->key >= key){
+        cur = cur->left;
+    }
+    else{
+        pre = cur;
+        cur = cur->right;
+    }
+}
+return;
+}
