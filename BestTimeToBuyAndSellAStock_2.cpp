907ad3ed92da0d7eaace3cd0da30d69fe81e58c7
PARANJAIK <97453375+PARANJAIK@users.noreply.github.com>
2023-03-24 19:38:49 +0530

    DynamicProgramming->Stocks

--- BestTimeToBuyAndSellAStock_2.cpp
+++ BestTimeToBuyAndSellAStock_2.cpp
@@ -49,10 +49,10 @@
     int maxProfit(vector<int>& prices){
         int n = prices.size();
         vector<int> next(2);
+        vector<int> cur(2);
         next[0] = 0;
         next[1] = 0;
         for(int i=n-1;i>=0;i--){
-            vector<int> cur(2);
             cur[0] = max(prices[i] + next[1],next[0]);
             cur[1] = max(-prices[i] + next[0],next[1]);
             next = cur;
