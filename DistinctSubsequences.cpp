96d86e8217886584b73a59e4af1a080c66a3e76b
PARANJAIK <97453375+PARANJAIK@users.noreply.github.com>
2023-03-25 20:09:47 +0530

    DynamicProgramming->Strings

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -30,6 +30,32 @@
         return memo[i][j];
     }
 
+    // iv) Solution to GFG variation of question -> https://practice.geeksforgeeks.org/problems/number-of-distinct-subsequences0909/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=number-of-distinct-subsequences0909
+
+    // Ans) 
+
+    // int M = 1e9+7;
+    
+    // int distinctSubsequences(string s){
+    //     int n = s.size();
+    //     unordered_map<char,long long int> mapp;
+    //     long long int ans = solve(0,s,mapp);
+    //     return (ans)%M;
+    // }
+    
+    // long long int solve(int i,string& s,unordered_map<char,long long int>& mapp){
+    //     int n = s.size();
+    //     if(i == n){
+    //         return 1;
+    //     }
+    //     long long int nextans = solve(i+1,s,mapp);
+    //     // getting ans
+    //     long long int ans = nextans*2 - mapp[s[i]];
+    //     // updating the mapp
+    //     mapp[s[i]] = nextans;
+    //     return ans;
+    // }
+
     // ii) My approach WHICH FAILED(INITIAL ATTEMPT)
 
     // int numDistinct(string s, string t) {
