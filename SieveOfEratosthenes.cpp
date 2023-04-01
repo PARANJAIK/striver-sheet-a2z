36bda2a04a2b0cd3a823c6e4170eb2cfa06eac56
PARANJAIK <97453375+PARANJAIK@users.noreply.github.com>
2023-04-01 22:46:53 +0530

    BitManipulation->NumberTheory

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,21 @@
+// Q) Count number of primes less than n -> https://leetcode.com/problems/count-primes/description/
+
+    // Ans) 
+
+    // i) Sieve of Eratosthenes:
+
+    int countPrimes(int n) {
+        vector<bool> prime(n,true);
+        if(n == 0 || n==1) return 0;
+        prime[0] = prime[1] = false;
+        int ans = 0;
+        for(int i=2;i<n;i++){
+            if(prime[i] == true){
+                ans++;
+                for(int j=2*i;j<n;j+=i){
+                    prime[j] = false;
+                }
+            }
+        }
+        return ans;
+    }
