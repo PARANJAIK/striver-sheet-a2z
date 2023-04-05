a0c2b7a2f6c5dc8f4319c2ccfd550edb9c23c94d
PARANJAIK <97453375+PARANJAIK@users.noreply.github.com>
2023-04-05 13:35:16 +0530

    Stacks->NGE->PGE->NLE->PLE->COOL

--- SumOfSubarrayRanges.cpp
+++ SumOfSubarrayRanges.cpp
@@ -1,4 +1,4 @@
-// Q) Sum Of Subarray ranges -> https://leetcode.com/problems/sum-of-subarray-ranges/description/
+ // Q) Sum Of Subarray ranges -> https://leetcode.com/problems/sum-of-subarray-ranges/description/
 
     // Ans) 
 
@@ -30,3 +30,41 @@
         }
         return ans;
     }
+
+    // ii) Tried to improve, I hoped it did, but realised it didn't, dont know why ?
+
+    // long long subArrayRanges(vector<int>& nums){
+    //     int n = nums.size();
+    //     stack<int> st;
+    //     stack<int> stt;
+    //     long long int ans = 0;
+    //     for(int i=0;i<n;i++){
+    //         while(!st.empty() && nums[st.top()] >= nums[i]){
+    //             int cur = st.top();
+    //             st.pop();
+    //             int pleindex = (st.empty()) ? -1 : st.top();
+    //             ans -= (1LL*nums[cur]*(i-cur)*(cur-pleindex));
+    //         }
+    //         st.push(i);
+    //         while(!stt.empty() && nums[stt.top()] <= nums[i]){
+    //             int cur = stt.top();
+    //             stt.pop();
+    //             int pgeindex = (stt.empty()) ? -1 : stt.top();
+    //             ans += (1LL*nums[cur]*(i-cur)*(cur-pgeindex));
+    //         }
+    //         stt.push(i);
+    //     }
+    //     while(!st.empty()){
+    //         int cur = st.top();
+    //         st.pop();
+    //         int pleindex = (st.empty()) ? -1 : st.top();
+    //         ans -= (1LL*nums[cur]*(n-cur)*(cur-pleindex));
+    //     }
+    //     while(!stt.empty()){
+    //         int cur = stt.top();
+    //         stt.pop();
+    //         int pgeindex = (stt.empty()) ? -1 : stt.top();
+    //         ans += (1LL*nums[cur]*(n-cur)*(cur-pgeindex));
+    //     }
+    //     return ans;
+    // }
