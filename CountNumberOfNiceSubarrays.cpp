c4bc788632550df2c35f9ee1585dcc12b1757860
PARANJAIK <97453375+PARANJAIK@users.noreply.github.com>
2023-04-08 13:41:40 +0530

    SlidingWindow->2PointerApproach

--- CountNumberOfNiceSubarrays.cpp
+++ CountNumberOfNiceSubarrays.cpp
@@ -35,23 +35,37 @@
 
     // ii) My 2nd approach : 
 
-    int numberOfSubarrays(vector<int>& nums,int k){
-       return solve(nums,k) - solve(nums,k-1);
-    }
+    // int numberOfSubarrays(vector<int>& nums,int k){
+    //    return solve(nums,k) - solve(nums,k-1);
+    // }
 
-    int solve(vector<int>& nums,int k){
-         int i = 0;
-        int ans = 0;
-        int j = 0;
-        int odd = 0;
-        while(j < nums.size()){
-            if(nums[j]%2 == 1) odd++;
-            while(odd > k){
-                if(nums[i]%2 == 1) odd--;
-                i++;
-            }
-            ans += (j-i+1);
-            j++;
+    // int solve(vector<int>& nums,int k){
+    //      int i = 0;
+    //     int ans = 0;
+    //     int j = 0;
+    //     int odd = 0;
+    //     while(j < nums.size()){
+    //         if(nums[j]%2 == 1) odd++;
+    //         while(odd > k){
+    //             if(nums[i]%2 == 1) odd--;
+    //             i++;
+    //         }
+    //         ans += (j-i+1);
+    //         j++;
+    //     }
+    //     return ans;
+    // }
+
+    // iii) Other COOL approach : 
+
+    int numberOfSubarrays(vector<int>& A, int k) {
+        int res = 0, i = 0, count = 0, n = A.size();
+        for (int j = 0; j < n; j++) {
+            if (A[j] & 1)
+                --k, count = 0;
+            while (k == 0)
+                k += A[i++] & 1, ++count;
+            res += count;
         }
-        return ans;
+        return res;
     }
